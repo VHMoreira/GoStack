b5052d17bf54e91c19c23d012466215adac7240c
Vitor Henrique <vhsouzaeng@gmail.com>
2020-07-11 16:39:25 -0300

    conceito-dev:mobile: Diferenças do ReactJS

--- conceitos-dev/mobile/src/index.js
+++ conceitos-dev/mobile/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 
 
 function App() {
     return (
-        <View>
-            <Text>Hello World! Thats my new React Native App</Text>
-        </View>
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            <View style={styles.container}>
+                <Text style={styles.title}>Hello World!</Text>
+            </View>
+        </>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: "center",
+        backgroundColor: "#7159c1"
+    },
+    title: {
+        fontSize: 32,
+        color: "#FFFFFF"
+    }
+});
 
 export default App;
