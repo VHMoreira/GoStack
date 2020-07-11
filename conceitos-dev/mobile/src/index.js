9cc4fb5fb0be66bd435482db66fbb83c644d7fe8
Vitor Henrique <vhsouzaeng@gmail.com>
2020-07-11 16:29:03 -0300

    conceito-dev:mobile: First commit

---
+++ conceitos-dev/mobile/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from "react-native";
+
+
+function App() {
+    return (
+        <View>
+            <Text>Hello World! Thats my new React Native App</Text>
+        </View>
+    );
+}
+
+export default App;
