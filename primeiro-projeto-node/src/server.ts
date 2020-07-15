84f738020b5168418b3e5a47fec526d2db8dd906
Vitor Henrique <vhsouzaeng@gmail.com>
2020-07-15 18:09:41 -0300

    primeiro-projeto-node: Padrões de Projeto com Eslint, Prettier e Editor Config

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
+// eslint-disable-next-line prettier/prettier
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello Vitor' });
-});
+app.get('/', (request, response) => response.json({ message: 'Hello Vitor' }));
 
 app.listen(3333, () => {
-    console.log("Server started!!");
-})
+  console.log('Server started!!');
+});
