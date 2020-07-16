6efd01383be55a020c38829e7f4853e530f80372
Vitor Henrique <vhsouzaeng@gmail.com>
2020-07-16 13:39:56 -0300

    desafios: desafio-fundamentos-nodejs finalizado

---
+++ desafios/desafio-fundamentos-nodejs/src/services/CreateTransactionService.ts
@@ -0,0 +1,32 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string,
+  value: number,
+  type: 'income' | 'outcome'
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+
+      if (balance.total < value) {
+        throw Error("You don't have amount to make this transaction");
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({ title, value, type });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
