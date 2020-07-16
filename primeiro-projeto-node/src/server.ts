0fc5349158209b64ac7495c0f1bd4363e3e59a45
Vitor Henrique <vhsouzaeng@gmail.com>
2020-07-16 09:46:06 -0300

    primeiro-projeto-node: Cadastro de Agendamentos

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line prettier/prettier
 import express from 'express';
 import routes from './routes';
 
@@ -8,6 +7,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  // eslint-disable-next-line no-console
   console.log('Server started!!');
 });
