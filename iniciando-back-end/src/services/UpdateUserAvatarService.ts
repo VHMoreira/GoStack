b889effcc4e91cfe64584c1690ebd13f48982ff9
Vitor Henrique <vhsouzaeng@gmail.com>
2020-07-23 08:36:14 -0300

    iniciando-back-end: Criando classe de erro

--- iniciando-back-end/src/services/UpdateUserAvatarService.ts
+++ iniciando-back-end/src/services/UpdateUserAvatarService.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import fs from 'fs';
 import User from '../models/User';
 import uploadConfig from '../config/upload';
+import AppError from '../errors/AppError';
 
 interface Request {
   user_id: string;
@@ -15,10 +16,8 @@ class UpdateUserAvatarService {
 
     const user = await usersRepository.findOne(user_id);
 
-    console.log(user);
-
     if (!user) {
-      throw new Error('Only authenticated user can change avatar.');
+      throw new AppError('Only authenticated user can change avatar.', 401);
     }
 
     if (user.avatar !== null) {
