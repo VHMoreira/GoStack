efe170015eacacc10974d71e6e9b982e77d981d6
Vitor Henrique <vhsouzaeng@gmail.com>
2020-07-28 12:50:39 -0300

    iniciando-front-end: Mantendo usuario no storage

--- iniciando-front-end/src/pages/Signin/index.tsx
+++ iniciando-front-end/src/pages/Signin/index.tsx
@@ -19,7 +19,9 @@ interface SignInFormData {
 const Signin: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useContext(AuthContext);
+  const { signIn, user } = useContext(AuthContext);
+
+  console.log(user);
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
