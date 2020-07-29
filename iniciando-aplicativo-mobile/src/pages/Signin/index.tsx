2a2fbfc8608a2714b552e99840142a2840ca9558
Vitor Henrique <vhsouzaeng@gmail.com>
2020-07-29 14:44:28 -0300

    iniciando-aplicativo-mobile: Densidade de pixel

--- iniciando-aplicativo-mobile/src/pages/Signin/index.tsx
+++ iniciando-aplicativo-mobile/src/pages/Signin/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
 
 const Signin: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+    </Container>
+  );
 };
 
 export default Signin;
