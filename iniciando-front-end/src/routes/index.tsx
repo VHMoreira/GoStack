53219305a9dec21e14704e3b191a8313357d1d8b
Vitor Henrique <vhsouzaeng@gmail.com>
2020-07-28 22:35:02 -0300

    iniciando-front-end: Rotas autenticadas

--- iniciando-front-end/src/routes/index.tsx
+++ iniciando-front-end/src/routes/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
+import Route from "./Route";
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Signin} />
     <Route path="/signup" exact component={Signup} />
+    <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
 );
 
