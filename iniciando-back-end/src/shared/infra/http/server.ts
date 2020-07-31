cd78b59d80f163bc52a48c6d6729c208258cf4ca
Vitor Henrique <vhsouzaeng@gmail.com>
2020-07-30 21:04:32 -0300

    Arquitetura e testes no Node.js: Configurando imports

--- iniciando-back-end/src/shared/infra/http/server.ts
+++ iniciando-back-end/src/shared/infra/http/server.ts
@@ -2,10 +2,10 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import '@shared/infra/typeorm';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
-import '../../typeorm';
-import uploadConfig from '../../../../config/upload';
-import AppError from '../../../errors/AppError';
 
 const app = express();
 
