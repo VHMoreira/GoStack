30ab8a17bba212387ac198c6b157b7a7d3d12faa
Vitor Henrique <vhsouzaeng@gmail.com>
2020-08-01 16:04:01 -0300

    Arquitetura e testes no Node.js: Usando controllers

---
+++ iniciando-back-end/src/modules/users/infra/http/controllers/UsersController.ts
@@ -0,0 +1,17 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+import CreateUserservice from '@modules/users/services/CreateUserService';
+
+export default class UsersController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUser = container.resolve(CreateUserservice);
+
+    const user = await createUser.execute({ name, email, password });
+
+    delete user.password;
+
+    return response.json(user);
+  }
+}
