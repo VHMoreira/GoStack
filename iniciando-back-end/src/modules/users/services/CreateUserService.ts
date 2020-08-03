7899c3654752ed32073d5b8c354163971e8e0265
Vitor Henrique <vhsouzaeng@gmail.com>
2020-08-03 15:01:14 -0300

    Arquitetura e testes no Node.js: Testando autenticaçao

--- iniciando-back-end/src/modules/users/services/CreateUserService.ts
+++ iniciando-back-end/src/modules/users/services/CreateUserService.ts
@@ -1,8 +1,8 @@
-import { hash } from 'bcryptjs';
 import { injectable, inject } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
 import IUserRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface Request {
   name: string;
@@ -14,6 +14,7 @@ interface Request {
 class CreateUserservice {
   constructor(
     @inject('UsersRepository') private usersRepository: IUserRepository,
+    @inject('HashProvider') private hashProvider: IHashProvider,
   ) { }
 
   public async execute({ name, email, password }: Request): Promise<User> {
@@ -23,7 +24,7 @@ class CreateUserservice {
       throw new AppError('Email address already used.');
     }
 
-    const hashedPassword = await hash(password, 8);
+    const hashedPassword = await this.hashProvider.generateHash(password);
 
     const user = this.usersRepository.create({
       name,
