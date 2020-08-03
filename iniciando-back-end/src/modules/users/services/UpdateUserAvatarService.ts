1849528e3f1ab766454ea248a4d57c9b772d5db5
Vitor Henrique <vhsouzaeng@gmail.com>
2020-08-03 15:45:19 -0300

    Arquitetura e testes no Node.js: Provider de Storage

--- iniciando-back-end/src/modules/users/services/UpdateUserAvatarService.ts
+++ iniciando-back-end/src/modules/users/services/UpdateUserAvatarService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import fs from 'fs';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import IStorageProvider from '@shared/container/provider/StorageProvider/models/IStorageProvider';
 import User from '../infra/typeorm/entities/User';
 import IUserRepository from '../repositories/IUsersRepository';
 
@@ -15,6 +16,7 @@ interface IRequest {
 class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository') private usersRepository: IUserRepository,
+    @inject('StorageProvider') private storageProvider: IStorageProvider,
   ) { }
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -25,16 +27,12 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar !== null) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
