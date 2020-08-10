113d3f1460aca44e9820423348b19731f600a1b6
Vitor Henrique <vhsouzaeng@gmail.com>
2020-08-10 16:30:28 -0300

    Finalizando o back-end do app: Cache lista de providers

--- iniciando-back-end/src/modules/appointments/services/ListProvidersService.ts
+++ iniciando-back-end/src/modules/appointments/services/ListProvidersService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/provider/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   user_id: string;
@@ -10,12 +11,24 @@ interface IRequest {
 class ListProvidersService {
   constructor(
     @inject('UsersRepository') private usersRepository: IUserRepository,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) { }
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `roviders-list:${user_id}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+
+      console.log('A query no banco foi feita');
+
+      await this.cacheProvider.save(`roviders-list:${user_id}`, users);
+    }
 
     return users;
   }
