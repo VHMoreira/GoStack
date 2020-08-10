8f99a4df280e1c31bd2cd225771df674022b5460
Vitor Henrique <vhsouzaeng@gmail.com>
2020-08-10 16:08:15 -0300

    Finalizando o back-end do app: Cache lista de providers

--- iniciando-back-end/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ iniciando-back-end/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
+import ICacheProvider from '@shared/container/provider/CacheProvider/models/ICacheProvider';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -15,6 +16,9 @@ class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) { }
 
   public async execute({
@@ -23,9 +27,14 @@ class ListProviderAppointmentsService {
     month,
     year,
   }: IRequest): Promise<Appointment[]> {
+    const cacheDate = await this.cacheProvider.recover('hs');
+
+    console.log(cacheDate);
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
       { provider_id, day, month, year },
     );
+
+    // await this.cacheProvider.save('hs', 'hshssss');
 
     return appointments;
   }
