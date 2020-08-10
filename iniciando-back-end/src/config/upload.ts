b2972f9a0c056e6b3f045e80b434df13ddb35ee5
Vitor Henrique <vhsouzaeng@gmail.com>
2020-08-10 09:47:19 -0300

    Finalizando o back-end do app: Upload de arquivos para o Amazon S3

--- iniciando-back-end/src/config/upload.ts
+++ iniciando-back-end/src/config/upload.ts
@@ -1,19 +1,43 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
+
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tempFolder: string;
+  uploadsFolder: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 const tempFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 export default {
+  driver: process.env.STORAGE_DRIVER,
   tempFolder,
   uploadsFolder: path.resolve(tempFolder, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tempFolder,
-    filename: (request, file, callback) => {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const filename = `${fileHash}-${file.originalname}`;
+  multer: {
+    storage: multer.diskStorage({
+      destination: tempFolder,
+      filename: (request, file, callback) => {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const filename = `${fileHash}-${file.originalname}`;
 
-      return callback(null, filename);
+        return callback(null, filename);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'gostack-gobarber-vitor',
     },
-  }),
-};
+  },
+} as IUploadConfig;
