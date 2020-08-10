c17d057fba21c5ff8e30bcfa7d93d3fe7b582a3b
Vitor Henrique <vhsouzaeng@gmail.com>
2020-08-10 16:44:51 -0300

    Finalizando o back-end do app: Invalidando cache

--- iniciando-back-end/src/modules/appointments/services/ListProvidersService.ts
+++ iniciando-back-end/src/modules/appointments/services/ListProvidersService.ts
@@ -17,8 +17,10 @@ class ListProvidersService {
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
     let users = await this.cacheProvider.recover<User[]>(
-      `roviders-list:${user_id}`,
+      `providers-list:${user_id}`,
     );
+
+    console.log(users);
 
     if (!users) {
       users = await this.usersRepository.findAllProviders({
@@ -27,7 +29,7 @@ class ListProvidersService {
 
       console.log('A query no banco foi feita');
 
-      await this.cacheProvider.save(`roviders-list:${user_id}`, users);
+      await this.cacheProvider.save(`providers-list:${user_id}`, users);
     }
 
     return users;
