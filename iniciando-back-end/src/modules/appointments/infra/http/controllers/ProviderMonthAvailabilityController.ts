20f11db66bf887949ee77bcc5f9f3e7535ab5287
Vitor Henrique <vhsouzaeng@gmail.com>
2020-08-11 20:13:46 -0300

    finzalizando front-end web do app: Dias indisponiveis no mes

--- iniciando-back-end/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ iniciando-back-end/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -5,7 +5,7 @@ import ListProviderMonthAvailabityService from '@modules/appointments/services/L
 export default class ProviderMonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { month, year } = request.body;
+    const { month, year } = request.query;
 
     const listProviderMonthAvailabity = container.resolve(
       ListProviderMonthAvailabityService,
