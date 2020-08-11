971ca4e5767f394dda9f4ab8cafff1ce8248584c
Vitor Henrique <vhsouzaeng@gmail.com>
2020-08-11 20:37:49 -0300

    finzalizando front-end web do app: Serializaçao no cache

--- iniciando-back-end/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ iniciando-back-end/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
 import ICacheProvider from '@shared/container/provider/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -37,7 +38,7 @@ class ListProviderAppointmentsService {
         { provider_id, day, month, year },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
