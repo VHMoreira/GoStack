5f651bec1aacb352d5d164912389e3a9c0358c1e
Vitor Henrique <vhsouzaeng@gmail.com>
2020-08-11 21:48:40 -0300

    finzalizando front-end web do app: Criando pagina de redefiniçao

--- iniciando-front-end/src/routes/index.tsx
+++ iniciando-front-end/src/routes/index.tsx
@@ -6,12 +6,14 @@ import Signup from '../pages/Signup';
 import Route from "./Route";
 import Dashboard from '../pages/Dashboard';
 import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Signin} />
     <Route path="/signup" exact component={Signup} />
     <Route path="/forgot-password" exact component={ForgotPassword} />
+    <Route path="/reset-password" exact component={ResetPassword} />
     <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
 );
